3b5a1885023acb078cd2ddf8399d2f84b4cab806
bingo2019 <274569314@qq.com>
2021-06-01 11:44:37 +0800

    A file will push to github master breach

--- gitpush.sh
+++ gitpush.sh
@@ -6,6 +6,10 @@ else
 fi
 git add $GIT_PUSH_FILE_NAME
 git commit -m "A file will push to github master breach "
-git push origin master
 
+if [ "$2" ];then
+   export GIT_PUSH_TO_CLOUD=$2
+   git push origin master
+else
+   echo "git success"
 
